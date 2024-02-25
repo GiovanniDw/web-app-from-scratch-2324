af7aa7876687ddbd82396acda0df3c007a21bb3f
Giovanni D <10939082+GiovanniDw@users.noreply.github.com>
2024-02-25 12:01:45 +0100

    cleanup and added build

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig, loadEnv } from 'vite';
+
+// export default defineConfig({
+//   server: {
+//     port: 3000,
+//   },
+//   preview: {
+//     port: 8080,
+//   },
+// });
+
+
+export default defineConfig({
+  base: "/web-app-from-scratch-2324/",
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
+  build: {
+    outDir: 'docs',
+    sourcemap: true,
+    minify: false,
+    optimizeDeps: {
+    include: ['./src/js/vendor/routie.js'],
+    },
+  }
+},({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  }
+})
